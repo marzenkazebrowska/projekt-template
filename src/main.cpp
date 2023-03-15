ff934bc9b5555a1070acf3bedb99328895233f95
marzenkazebrowska <pbx33810@student.wsb.bydgoszcz.pl>
2023-03-15 18:27:13 +0100

    funkcja main dla TRÓJKĄT PASCALA

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-
 #include "code.hpp"
 
 int main(int argc, char** argv) {
-  // TODO: main program function
+    // Generowanie trójkąta Pascala dla 5 wierszy
+    std::vector<std::vector<int>> pascalTriangle = generatePascalTriangle(5);
+
+    // Wyświetlenie trójkąta Pascala
+    for (const auto& row : pascalTriangle) {
+        for (const auto& elem : row) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
+
