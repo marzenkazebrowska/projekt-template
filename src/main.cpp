29ce3f97ec255dfd7899b2f97ae88e6e31f93a1a
marzenkazebrowska <pbx33810@student.wsb.bydgoszcz.pl>
2023-03-15 20:07:21 +0100

    zmiany w funkcji main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-#include "code.hpp"
 
-int main(int argc, char** argv) {
+#include "code.hpp"
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> generatePascalTriangle(int numRows);
+
+int main() {
     // Generowanie trójkąta Pascala dla 5 wierszy
     std::vector<std::vector<int>> pascalTriangle = generatePascalTriangle(5);
 
@@ -14,5 +18,20 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+std::vector<std::vector<int>> generatePascalTriangle(int numRows) {
+    std::vector<std::vector<int>> triangle(numRows);
+
+    for (int i = 0; i < numRows; i++) {
+        triangle[i].resize(i + 1);
+        triangle[i][0] = triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    return triangle;
 }
 
