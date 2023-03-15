890550693d20f7d332aaf959b0c73e4be8bad5cd
marzenkazebrowska <pbx33810@student.wsb.bydgoszcz.pl>
2023-03-15 18:17:33 +0100

    zmiana code.cpp

--- src/code.cpp
+++ src/code.cpp
@@ -1,3 +1,6 @@
-#include "code.hpp"
+#include "TPascala.hpp"
 
-// TODO: testable function definition
+// Napisz program, który wyświetli trójkąt Pascala //
+//od n=0 do n=10. //
+//W tym celu stwórz funkcję //
+//obliczającą dwumian Newtona //
