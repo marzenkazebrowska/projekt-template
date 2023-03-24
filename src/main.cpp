425ff5e0607de2930ee8e7d557d6f61c26555b97
marzenkazebrowska <127055176+marzenkazebrowska@users.noreply.github.com>
2023-03-25 00:20:46 +0100

    main change

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include "code.h"
 
-int main(int argc, char* argv[]) {
-    std::cout << hello("Unnamed") << std::endl;
-    return 0;
-}
+std::cout << hello("Unnamed") << std::endl;
+
