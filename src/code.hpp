349e83764e21817b968efb2452da468aee6bc446
marzenkazebrowska <127055176+marzenkazebrowska@users.noreply.github.com>
2023-03-25 00:02:36 +0100

    code.cpp change

--- src/code.hpp
+++ src/code.hpp
@@ -3,13 +3,4 @@
 #include <string>
 #include <string_view>
 
-std::string hello(std::string_view)
-// TODO: testable function declaration
-
-
-
-// Funkcja generująca trójkąt Pascala do określonego poziomu
-// TPascala1.cpp/
-
-
-
+std::string hello(std::string_view);
