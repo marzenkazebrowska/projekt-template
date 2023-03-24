a823a0323b085ec17a5c2cd343bb33f35148f981
marzenkazebrowska <127055176+marzenkazebrowska@users.noreply.github.com>
2023-03-25 00:12:35 +0100

    main change

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,8 @@
 
 #include "code.hpp"
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout <<hello("Unnamed") << std::end1;
-    } else {
-        std::cout << hello(argv[1]) << std::endl;
-    }
+int main(int argc, char* argv[]) {
+    std::cout << hello("Unnamed") << std::end1;
+    return 0;
 }
+    
