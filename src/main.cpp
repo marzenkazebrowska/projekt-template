76fe92206ec5f075627a4688d39d4e20bca1c2d4
marzenkazebrowska <127055176+marzenkazebrowska@users.noreply.github.com>
2023-03-25 10:54:31 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-
 #include "code.hpp"
 
+using namespace std;
+
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout <<hello("Unnamed") << std::end1;
-    } else {
-        std::cout << hello(argv[1]) << std::endl;
-    }
+  if (argc < 2) {
+    cout << hello("Unnamed") << endl;
+  } else {
+    cout << hello(argv[1]) << endl;
+  }
 }
